dd2943ae849f4000a6b0c33bc942ffaac62053a8
Mikel Cox <psn.power@yahoo.com>
2026-02-12 14:50:11 -0500

    Updated vite.config.ts and .gitignore, added gh-pages dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:239a7ae627314b987bca8fe2f2bd93aa4dfb4eb77a55cbaef68765e86455003c
-size 253
+oid sha256:aba72de7c25689a53fc1650f75816309855796935d3b466ee106366763d67afd
+size 344
