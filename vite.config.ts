1ac9dbc233e66084b5f476ec86271aeeb5b7f2b7
Mikel Cox <psn.power@yahoo.com>
2026-02-12 14:50:11 -0500

    Updated vite.config.ts and .gitignore, added gh-pages dependency

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'build',
+    emptyOutDir: true, // also necessary
+  },
   plugins: [
     react({
       babel: {
