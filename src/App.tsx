8d550463a2a1184f73457deea24ade0fa5c9c47a
Mikel Cox <psn.power@yahoo.com>
2026-02-12 19:38:36 -0500

    Added my haiku

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,23 @@ import './App.css';
 function App() {
   
   return (
-    <>
-      <div>
-        <h1>Mikel & Audrey</h1>
-      </div>
-      <div className="card">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
+	<>
+		<div>
+			<h1>Mikel & Audrey</h1>
+			<span><center>
+				Roses, they are red.
+				Beautiful like you, they are.
+				That's why I pick you.
+			</center></span>
+		</div>
+		<div>
+		</div>
+		<div className="card">
+			<p>
+				Edit <code>src/App.tsx</code> and save to test HMR
+			</p>
+		</div>
+	</>
   )
 };
 
