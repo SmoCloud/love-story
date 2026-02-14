9000159869a9073db53a0dab2a02ef172f9528df
Mikel Cox <psn.power@yahoo.com>
2026-02-14 08:29:57 -0500

    What the heck is going on, why can vite not serve the static files?

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   root: '.',
-  base: './',
   build: {
     outDir: 'build',
     emptyOutDir: true, // also necessary
