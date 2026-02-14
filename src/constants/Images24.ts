37d0a33b92deb00edcb0a92cf1db4c60c160ce6c
Mikel Cox <psn.power@yahoo.com>
2026-02-14 08:20:56 -0500

    Removing ts imports and making the file objects static array.

--- src/constants/Images24.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1592719cd922a8c749be11a2d72263dbc7ad238d0c50237504c33c96ed6013c2
-size 5118
--- src/constants/Images25.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:16c5f4fdd9e4d42ede69943656ef413f5201403196b1425d19d24fa2846999b8
-size 1956
