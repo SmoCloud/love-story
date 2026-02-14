b258df82a67653be750917739d62bbee536cd5c5
Mikel Cox <psn.power@yahoo.com>
2026-02-14 08:20:56 -0500

    Removing ts imports and making the file objects static array.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7af357d5a355a986567d2d2523868e23b3a9a870eb94b373ad822549b01a7a96
-size 8782
+oid sha256:025544e6ec4f57360d185c36b9c0c59b4fc295b2d34f57d67472a89bce0efc9b
+size 16239
--- src/constants/Videos25.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6e07036ab99a54203cedfcf3cb66e4836942901313e737d1460f4b789d70ad26
-size 428
