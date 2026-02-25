0a90b25d87f2a7e0f5cc0640ae02ee10874e6383
Mikel Cox <psn.power@yahoo.com>
2026-02-25 09:42:03 -0500

    Images first, then the video. Hopefully LFS works.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e51112eb3e57e1ea232b129b02020c383f60da142a9368c912e7ff6ceef5dd7a
-size 16236
+oid sha256:9c9ee4cd43d02c5935c8458716354bc969465fd9ded4366a8b87e3775594dbc5
+size 17422
